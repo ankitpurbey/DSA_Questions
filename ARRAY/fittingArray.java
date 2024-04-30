9e1404904728eaa4b06dc059be5da6cafb5ea893
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-04-30 22:19:08 +0530

    Update fittingArray.java

--- ARRAY/fittingArray.java
+++ ARRAY/fittingArray.java
@@ -1,5 +1,17 @@
 package ARRAY;
 
+import java.util.Arrays;
+
 public class fittingArray {
-    
+    public static boolean fitOrNot(int arr[],int brr[]){
+        Arrays.sort(arr);
+        Arrays.sort(brr);
+        for(int i=0; i<arr.length; i++){
+            if()
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] ={2,4,6,3};
+        int brr[] ={3,1,6,4};
+    }
 }
