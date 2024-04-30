c8ef6b34b0a03c6c22804cfd46b2844441a7079b
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-04-30 22:56:34 +0530

    Create subsetArr.java

---
+++ ARRAY/subsetArr.java
@@ -0,0 +1,14 @@
+package ARRAY;
+
+public class subsetArr {
+    public static boolean subsetOrNot(int arr[],int arr2[]){
+        for(int i=0; i<arr.length; i++){
+            
+            if(arr[i])
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] ={2,3,1,5,7};
+        int arr2[] ={4,1,6,8,5,9};
+    }
+}
