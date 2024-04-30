306e9d500af5ce225920aa05ffc1b6a5bbe99da7
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-04-30 22:58:42 +0530

    Update subsetArr.java

--- ARRAY/subsetArr.java
+++ ARRAY/subsetArr.java
@@ -1,11 +1,19 @@
 package ARRAY;
 
+import java.util.Arrays;
+
 public class subsetArr {
     public static boolean subsetOrNot(int arr[],int arr2[]){
+        Arrays.sort(arr);
+        Arrays.sort(arr2);
         for(int i=0; i<arr.length; i++){
-            
-            if(arr[i])
+            for(int j=0; j<arr2.length; j++){
+            if(arr[i]!=arr[j]){
+                return false;
+            }
         }
+        }
+        
     }
     public static void main(String[] args) {
         int arr[] ={2,3,1,5,7};
