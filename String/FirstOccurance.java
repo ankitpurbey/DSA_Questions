9068ab3690432ab49bc8f528bd97a37719ff4f55
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-05-02 21:48:54 +0530

    Update FirstOccurance.java

--- String/FirstOccurance.java
+++ String/FirstOccurance.java
@@ -1,5 +1,8 @@
 package String;
 
 public class FirstOccurance {
-    
+    public static String fstOccurance(String)
+    public static void main(String[] args) {
+        String str ="abhgsb";
+    }
 }
