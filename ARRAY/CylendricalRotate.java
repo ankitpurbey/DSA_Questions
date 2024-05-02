7f5799def5a5b22864bc7572d1d50434e3fa66fc
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-05-02 19:03:47 +0530

    Update CylendricalRotate.java

--- ARRAY/CylendricalRotate.java
+++ ARRAY/CylendricalRotate.java
@@ -1,7 +1,14 @@
 package ARRAY;
 
 public class CylendricalRotate {
+    public static void Rotate(int arr[],int n){
+        for(int i=n-1; i>0; i--){
+            int temp = arr[i];
+            
+        }
+    }
     public static void main(String[] args) {
-        
+        int arr[] ={1,2,3,4,5};
+        int n= arr.length;
     }
 }
