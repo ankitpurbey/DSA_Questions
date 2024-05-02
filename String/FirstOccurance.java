67b31524437ec4d0df7403b4d8366449ac808421
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-05-02 21:51:23 +0530

    Update FirstOccurance.java

--- String/FirstOccurance.java
+++ String/FirstOccurance.java
@@ -1,8 +1,19 @@
 package String;
 
 public class FirstOccurance {
-    public static String fstOccurance(String)
+    public static String fstOccurance(String str){
+        char s ='';
+        for(int i=0; i<str.length(); i++){
+            for(int j=0; j>str.length(); j++){
+                if(str.charAt(i) ==str.charAt(j)){
+                    s = charAt(i);
+                }
+            }
+        }
+        return s;
+    }
     public static void main(String[] args) {
         String str ="abhgsb";
+        System.out.println(fstOccurance(str));
     }
 }
