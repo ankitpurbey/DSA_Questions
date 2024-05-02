f7927201f9add3a53a1f344f34c5ca524ec1a214
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-05-02 13:25:23 +0530

    Update maxInStructArr.java

--- ARRAY/maxInStructArr.java
+++ ARRAY/maxInStructArr.java
@@ -2,9 +2,15 @@ package ARRAY;
 
 public class maxInStructArr {
     public static findMax(int arr[]){
-        for(int i=0; )
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<n; i++){
+            int t = (arr[i].feet)*12 + (arr[i].inches);
+            max = Math.max(max,t);
+        }
+        return max;
     }
     public static void main(String[] args) {
         int arr[] ={1,2,2,1};
+        System.out.printl(findMax);
     }
 }
