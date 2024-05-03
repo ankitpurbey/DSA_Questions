8c0f995c8c18eb726ee94a6adda3584a4ae114c1
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-05-03 11:25:29 +0530

    s

---
+++ Stack/insert.java
@@ -0,0 +1,15 @@
+package Stack;
+
+import java.util.ArrayList;
+
+public class insert {
+    static class Stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+        public static boolean(isEmpty()){
+            return list.size()==0;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
--- String/FirstOccurance.java
+++ String/FirstOccurance.java
@@ -1,16 +1,16 @@
 package String;
 
 public class FirstOccurance {
-    public static String fstOccurance(String str){
-        char s ='';
+    public static int fstOccurance(String str){
+        //char s =' ';
         for(int i=0; i<str.length(); i++){
             for(int j=0; j>str.length(); j++){
                 if(str.charAt(i) ==str.charAt(j)){
-                    s = charAt(i);
+                    return charAt(i);
                 }
             }
         }
-        return s;
+        return -1;
     }
     public static void main(String[] args) {
         String str ="abhgsb";
