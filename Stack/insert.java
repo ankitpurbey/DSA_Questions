99bc1534aab78fbb45dea4267def409827b9d93b
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-05-03 11:29:20 +0530

    Update insert.java

--- Stack/insert.java
+++ Stack/insert.java
@@ -8,6 +8,16 @@ public class insert {
         public static boolean(isEmpty()){
             return list.size()==0;
         }
+        //push
+        public static void push(int data){
+            list.add(data);
+        }
+        //pop
+        public static int pop(){
+            int top =list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
     }
     public static void main(String[] args) {
         
