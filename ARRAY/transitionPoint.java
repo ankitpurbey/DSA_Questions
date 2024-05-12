87624c7171b1576acc28312767a5fdce1b781311
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-05-13 02:41:14 +0530

    Create transitionPoint.java

---
+++ ARRAY/transitionPoint.java
@@ -0,0 +1,12 @@
+package ARRAY;
+
+public class transitionPoint {
+    public int tpoint(int arr[]{
+        for()
+    })
+    public static void main(String[] args) {
+        int arr[] ={0,0,0,1,0,1,1};
+
+    }
+    
+}
