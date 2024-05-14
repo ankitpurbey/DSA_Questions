662941da0006427d1948e316b2e48de4238446af
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-05-15 02:44:41 +0530

    Update firstLetterOfEveryWord.java

--- String/firstLetterOfEveryWord.java
+++ String/firstLetterOfEveryWord.java
@@ -2,7 +2,14 @@ package String;
 
 public class firstLetterOfEveryWord {
     public static int firstLetter(String s){
-        
+        String a = s.charAt(0)+"";
+        for(int i=0; i<s.length(); i++){
+            if (s.charAt(i)=' ') {
+                a += s.charAt(i+1);
+                
+            }
+        }
+        return a;
     }
     public static void main(String[] args) {
         String s = "I m Ankit";
