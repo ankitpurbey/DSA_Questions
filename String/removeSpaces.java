1525b6eb8096e191bb1742eec8efe0107d97f833
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-05-16 02:19:36 +0530

    Update removeSpaces.java

--- String/removeSpaces.java
+++ String/removeSpaces.java
@@ -5,10 +5,14 @@ public class removeSpaces {
         // Solve using String Builder 
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<str.length(); i++){
-            
+            if (str.charAt(i)!= ' ') {
+                sb.append(str.charAt(i));
+            }
         }
+        return sb.toString();
     }
     public static void main(String[] args) {
         String str ="i m Ankit ";
+        System.out.println(rSpaces(str));
     }
 }
