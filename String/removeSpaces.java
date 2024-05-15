d4fd60539e4f9f48c0b277e2e17715a01b5aa134
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-05-16 02:16:13 +0530

    Create removeSpaces.java

---
+++ String/removeSpaces.java
@@ -0,0 +1,14 @@
+package String;
+
+public class removeSpaces {
+    public static String rSpaces(String str){
+        // Solve using String Builder 
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<str.length(); i++){
+            
+        }
+    }
+    public static void main(String[] args) {
+        String str ="i m Ankit ";
+    }
+}
