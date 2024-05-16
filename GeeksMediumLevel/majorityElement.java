838454f06ad73695d804323b9ff01e11899a90e3
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-05-17 01:04:44 +0530

    Update majorityElement.java

--- GeeksMediumLevel/majorityElement.java
+++ GeeksMediumLevel/majorityElement.java
@@ -1,7 +1,18 @@
 package GeeksMediumLevel;
 
+import java.util.Arrays;
+
 public class majorityElement {
+    public static int majority(int arr[],int n){
+        int count =1;
+        int b = n/2;
+        Arrays.sort(arr);
+        for(int i=0; i<n; i++){
+            if(arr[i]==arr[i+1])
+        }
+    }
     public static void main(String[] args) {
-        int arr[] =
+        int arr[] ={3,1,3,3,2};
+        int n = arr.length;
     }
 }
