17b69ee26aa728d48074cf9ad9ca1330aad878dc
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-06-03 23:06:58 +0530

    Create kLargest.java

---
+++ ARRAY/kLargest.java
@@ -0,0 +1,16 @@
+package ARRAY;
+
+import java.util.Arrays;
+
+public class kLargest {
+    public static int[] largest(int arr[],int k,int n) {
+    
+        int ans[] = new int[k];
+        Arrays.sort(arr);
+    }
+    public static void main(String[] args) {
+        int arr[] ={1,89,67,45,34,78};
+        int n = arr.length;
+        int k =3;
+    }
+}
