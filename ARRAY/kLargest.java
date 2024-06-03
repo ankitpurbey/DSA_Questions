ee4b70d10731de0c21e27b540819069de3e4f6ce
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-06-03 23:09:00 +0530

    Update kLargest.java

--- ARRAY/kLargest.java
+++ ARRAY/kLargest.java
@@ -7,10 +7,15 @@ public class kLargest {
     
         int ans[] = new int[k];
         Arrays.sort(arr);
+        for(int i=0; i<n; i++){
+            ans[i] = arr[n-1-i];
+        }
+        return ans;
     }
     public static void main(String[] args) {
         int arr[] ={1,89,67,45,34,78};
         int n = arr.length;
         int k =3;
+        System.out.println(largest(arr, k, n));
     }
 }
